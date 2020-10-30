84de4b72cfcd646357cf651006d20db1a9906209
Ron-it <70334468+Ron-it@users.noreply.github.com>
2020-10-30 10:10:26 +0800

    3.1

---
+++ dowhileLoop.java
@@ -0,0 +1,16 @@
+public class dowhileLoop
+{
+	
+	public static void main (String[] args) 
+	{
+		int x;
+do
+    {
+         x = IBIO.inputInt("enter a number less than 100 ");
+    } 
+        while (x >= 100);
+        IBIO.output("thank you");
+
+	}
+}
+
