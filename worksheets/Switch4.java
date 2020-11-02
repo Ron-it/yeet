01026b654ba81def19b12d75019111c3add1b6f7
Ron-it <70334468+Ron-it@users.noreply.github.com>
2020-11-02 13:22:17 +0800

    ‎

---
+++ worksheets/Switch4.java
@@ -0,0 +1,44 @@
+public class Switch4 
+{
+	
+    public static void main (String[] args)
+    {
+        int day = IBIO.inputInt("Enter a number (1-7): ");
+        String name;
+        switch( day )
+
+        {
+        case 1:
+            name = "Monday";
+            break;
+        
+        case 2:
+            name = "Tuesday";
+            break;
+        
+        case 3:
+            name = "Wednesday";
+            break;
+        
+        case 4:
+            name = "Thursday";
+            break;
+
+        case 5:
+            name = "Friday";
+            break;
+
+        case 6:
+            name = "Saturday";
+            break;
+
+        case 7:
+            name = "Sunday";
+            break;
+        default:
+            name = "error";
+            break;
+        }
+    System.out.println(day + " = " + name);
+    }
+}
