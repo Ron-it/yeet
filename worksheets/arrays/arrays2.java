875837b0c0bfd0b75891e51643717cb5583b5c58
Ron-it <ronitbuti.ofs@gmail.com>
2021-03-22 23:58:20 +0800

    ⠀

--- worksheets/arrays/arrays2.java
+++ worksheets/arrays/arrays2.java
@@ -1,139 +1,147 @@
 public class arrays2 {
-	
-	public static void main (String[] args) 
-	{
-		double grade[] = new double[8];
+
+    public static void main(String[] args) {
+        double grade[] = new double[8];
 
         System.out.println("Ronit");
         String subject = IBIO.inputString("Enter the subject name: ");
 
-         grade=storearray();
-        
-         IBIO.out(subject+": "+"| ");
-         display(grade);
+        grade = storearray();
+
+        IBIO.out(subject + ": " + "| ");
+        display(grade);
 
         IBIO.output("");
 
-         IBIO.output("Lowest: " + minimum(grade));
-         IBIO.output("Highest: " + maximum(grade));
-         double y = trend(grade);
+        IBIO.output("Lowest: " + minimum(grade));
+        IBIO.output("Highest: " + maximum(grade));
+        double y = trend(grade);
 
-         if(y>=4.0){
-             IBIO.output("Improving");
-         }
+        if (y >= 4.0) {
+            IBIO.output("Improving");
+        }
 
-         if(y<4.0){
-             IBIO.output("Worsening");
-         }
+        if (y < 4.0) {
+            IBIO.output("Worsening");
+        }
 
-         mode (grade);
-         median (grade);
-	}
+        mode(grade);
+        IBIO.output(median(grade));
+    }
 
-
-    public static void display(double[] grade ){
-        for(int i = 0; i<8; i++){
-            IBIO.out(grade[i]+ " | ");
+    public static void display(double[] grade) {
+        for (int i = 0; i < 8; i++) {
+            IBIO.out(grade[i] + " | ");
         }
     }
 
-    public static double maximum(double[] grade ){
+    public static double maximum(double[] grade) {
 
         double max = grade[0];
-        
-        for(int i = 1; i<8; i++){
 
-            if(grade[i]>max){
+        for (int i = 1; i < 8; i++) {
+
+            if (grade[i] > max) {
                 max = grade[i];
             }
         }
-return max;
+        return max;
     }
 
-    public static double minimum(double[] grade ){
+    public static double minimum(double[] grade) {
 
         double min = grade[0];
-        
-        for(int i = 1; i<8; i++){
 
-            if(grade[i]<min){
+        for (int i = 1; i < 8; i++) {
+
+            if (grade[i] < min) {
                 min = grade[i];
             }
         }
-return min;
+        return min;
     }
 
-
-    public static double trend(double[] grade){
+    public static double trend(double[] grade) {
 
         double total = 0.0;
         double average = 0.0;
 
-        for(int i = 0; i<8; i++){
+        for (int i = 0; i < 8; i++) {
             total = total + grade[i];
         }
 
-            average = (total/8);
-   return average;
- }
+        average = (total / 8);
+        return average;
+    }
 
-
-    public static void mode(double[] grade){
+    public static void mode(double[] grade) {
 
         int mode = 1;
         int max = 0;
-        double number=0;
+        double number = 0;
 
-        for(int i = 0; i<7; i++){
-            for(int n =i+1; n<8;n++){
-                if(grade[i]==grade[n]){
+        for (int i = 0; i < 7; i++) {
+            for (int n = i + 1; n < 8; n++) {
+                if (grade[i] == grade[n]) {
                     mode++;
                 }
-           
-                if(max<mode){
-                    max=mode;
+
+                if (max < mode) {
+                    max = mode;
                     number = grade[i];
                 }
 
             }
-       
+
         }
-  
-                IBIO.output(number + " occured " + max + " times.");
+
+        IBIO.output(number + " occured " + max + " times.");
     }
 
-    public static void median (double[] B) {
-	    
-	    double median = 0;
-	    
-	    median =  B[B.length/2];
-	    
-	    System.out.println("The median is = "+ median);
-	    
-	}
+    /*
+     * public static void median (double[] B) {
+     * 
+     * double median = 0;
+     * 
+     * median = B[B.length/2];
+     * 
+     * System.out.println("The median is = "+ median);
+     * 
+     * }
+     */
 
+    public static double median(double[] B) {
+        double median = 0;
+        if (B.length % 2 == 0) {
+            double val1 = B[B.length / 2];
+            double val2 = B[(B.length / 2) - 1];
+            median = (double) (val2 + val1) / 2;
+        } else {
+            median = B[(B.length / 2)];
+        }
+        return median;
+    }
 
-    public static double[] storearray(){
+    public static double[] storearray() {
 
-            double[] grade = new double[8];
-            int i = 0;
+        double[] grade = new double[8];
+        int i = 0;
 
-            while(i < 8){
+        while (i < 8) {
 
-                double x = IBIO.inputDouble("Enter grade #" + (i+1) +"?: ");
-                if((x>=1.0)&&(x<=7.9)){
-                    grade[i] = x;
-                    i++;
-                }
-                
-                else{
-                    IBIO.output("Error-grade must be between 1.0 and 7.9 inclusive.");
-                }
-
+            double x = IBIO.inputDouble("Enter grade #" + (i + 1) + "?: ");
+            if ((x >= 1.0) && (x <= 7.9)) {
+                grade[i] = x;
+                i++;
             }
 
-            return grade;
+            else {
+                IBIO.output("Error-grade must be between 1.0 and 7.9 inclusive.");
+            }
+
         }
 
+        return grade;
+    }
 
 }
---
+++ worksheets/practice stuff/Arrays.java
@@ -0,0 +1,86 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+ * int num[] = new int[10]; for(int i = 0 ; i<10 ; i++){
+ * 
+ * double val = Math.random()*6; int randomval = (int)(val+1); num[i] =
+ * randomval;
+ * 
+ * }
+ * 
+ * for(int i = 0; i<10; i++){
+ * 
+ * System.out.println("The values"+num[i]);
+ * 
+ * }
+ */// Print and store random numbers
+
+/*
+ * String s = IBIO.input("Enter the word: "); char[] x = s.toCharArray();
+ * for(int i = 0; i<x.length; i++){
+ * 
+ * IBIO.out(x[i]);
+ * 
+ * } System.out.println(" "); System.out.print("Reverse of the word is: ");
+ * 
+ * for(int i = x.length - 1; i>=0; i--){
+ * 
+ * System.out.print(x[i]);
+ * 
+ * }
+ */// Program prints and reverses a word
+
+/*
+ * int size = IBIO.inputInt("Enter the number of values: "); int[] numbers = new
+ * int[size];
+ * 
+ * for(int i = 0; i<size; i++){
+ * 
+ * numbers[i] = IBIO.inputInt("Enter value: ");
+ * 
+ * }
+ * 
+ * System.out.print("The even numbers enetered in the array are: ");
+ * 
+ * for(int i = 0; i<size; i++){
+ * 
+ * if(numbers[i]%2==0){ System.out.print(numbers[i] + ", "); }
+ * 
+ * }
+ */// Find even numbers in an array
+
+/*
+ * int max,min,range; int size = IBIO.inputInt("Enter the number of values");
+ * int[] numbers = new int [size];
+ * 
+ * for(int i = 0; i<size; i++){ numbers[i] = IBIO.inputInt("Enter values: "); }
+ * 
+ * max = numbers[0]; min = numbers[0];
+ * 
+ * for(int i = 1; i<size; i++){ if(max<numbers[i]){ max = numbers[i]; }
+ * 
+ * if(min>numbers[i]){ min = numbers[i]; }
+ * 
+ * }
+ * 
+ * range = max-min;
+ * 
+ * System.out.println("Highest value is: " + max);
+ * System.out.println("Smalles value is: " + min);
+ * System.out.println("The range is: " + range);
+ */
+// Find max and min and range from an array
+
+/*
+ * int size = IBIO.inputInt("Enter the number of values: "); int[] numbers = new
+ * int[size];
+ * 
+ * for(int i = 0; i<size; i++){ numbers[i] = IBIO.inputInt("Value: "); }
+ * 
+ * for(int i = 0; i<size; i++){ if((numbers[i]%2==0) && (numbers[i]%5==0)){
+ * //divisible by 2 and divisible by 5 System.out.print(numbers[i] + "   "); } }
+ */// finding mumbers div by 2 and 5
