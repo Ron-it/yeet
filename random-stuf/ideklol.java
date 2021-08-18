2cbd7c05574e1cec4b40b692377742d8f5d66cb2
Ron-it <ronitbuti.ofs@gmail.com>
2021-08-18 19:48:17 +0800

    random

--- random-stuf/ideklol.java
+++ random-stuf/ideklol.java
@@ -1,6 +1,13 @@
-public class ideklol{
+public class ideklol {
     public static void main (String[] args){
         System.out.println("Hello World");
         int x;
+        x.toString;
+    
+    public String toString() {
+        return ("{" + "}");
+    }
+        
+
     }
 }
---
+++ random-stuf/newy.java
@@ -0,0 +1,6 @@
+public class newy {
+
+    public static void main(String[] args) {
+        System.out.println("yeet");
+    }
+}
