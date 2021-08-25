d0768865f4c1f106ba1d4e9c9b33b142af178360
Ron-it <70334468+Ron-it@users.noreply.github.com>
2021-08-25 12:31:47 +0800

    Create robloc.java

---
+++ OOP/robloc/robloc.java
@@ -0,0 +1,31 @@
+public class robloc{
+    private String username;
+    private double robux;
+    private boolean old;
+    private static double robuxTax = 0.08;
+
+    public robloc() { //empty constructor
+
+    }
+
+    public robloc(String username, double robux, boolean old) {
+        this.username = username;
+        this.robux = robux;
+        this.old = old;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public static void main(String[] args) {
+        robloc fortnite = new robloc("fortnite", 120.0, true);
+        System.out.println(fortnite.getUsername());
+        
+    }
+}
