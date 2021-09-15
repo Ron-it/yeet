9b5e2585da98e0edccf9a360635b3bf84ddbec66
Ronit <ronitbuti.ofs@gmail.com>
2021-09-15 15:06:37 +0800

    bst

--- BST/BST.java
+++ BST/BST.java
@@ -1,25 +1,92 @@
-public class BST {
-    private BinaryNode rootNode;
+public class BST
+{
+    private BinaryNode root;
 
-    public BST(BinaryNode rootNode) {
-        this.rootNode = new BinaryNode(rootNode);
-        ;
+    //public BST(BinaryNode root) { this.root = root; }
+
+    public BST(int root)
+    {
+        this.root = new BinaryNode(root);
+        System.out.println("BST created with " + root + " as its root.");
     }
 
-    public boolean isEmpty() {
-        return this.rootNode == null;
+    public boolean isEmpty()
+    {
+        return this.root == null;
     }
 
-    public void add(Object Node) {
-        if (this.isEmpty()) {
-            this.rootNode = new BinaryNode(rootNode);
-            return;
+    public void add(BinaryNode node, BinaryNode newNode)
+    {
+        if (newNode.getData() < node.getData())
+        {
+            if (node.getLeft() != null)
+                add(node.getLeft(), newNode);
+            else
+            {
+                node.setLeft(newNode);
+                System.out.println(newNode + " added to the left of " + node);
+            }
+        } else if (newNode.getData() > node.getData())
+        {
+            if (node.getRight() != null)
+                add(node.getRight(), newNode);
+            else
+            {
+                node.setRight(newNode);
+                System.out.println(newNode + " added to the right of " + node);
+            }
         }
+    }
 
-        else if (this.rootNode == null) {
-            this.rootNode = new BinaryNode(rootNode);
-            return;
+    public void add(int data)
+    {
+        BinaryNode n = new BinaryNode(data);
+        add(this.root, n);
+    }
+
+    private void inOrder(BinaryNode node)
+    {
+        if (node.getLeft() != null)
+            inOrder(node.getLeft());
+        System.out.print(node + " ");
+        if (node.getRight() != null)
+            inOrder(node.getRight());
+    }
+
+        private void postOrder(BinaryNode node) {
+        if(node.getLeft() != null){
+            postOrder(node.getLeft());
         }
+        if(node.getRight() != null){
+            postOrder(node.getRight());
+        }
+        System.out.print(node + " ");
+    } 
+
+    private void preOrder(BinaryNode node){
+        System.out.print(node + " ");
+        if(node.getLeft() != null){
+            preOrder(node.getLeft());
+        }
+        if(node.getRight() != null){
+            preOrder(node.getRight());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        BST b = new BST(5);
+        b.add(12);
+        b.add(3);
+        b.add(9);
+        b.add(2);
+        System.out.print("InOrder traversal: \n");
+        b.inOrder(b.root);
+        System.out.print("preOrder traversal: \n");
+        b.preOrder(b.root);
+        System.out.print("postOrder traversal: \n");
+        System.out.println();
+        b.postOrder(b.root);
     }
 
 }
--- BST/BinaryNode.java
+++ BST/BinaryNode.java
@@ -1,48 +1,52 @@
-public class BinaryNode {
-    private Object data;// can be replaced by any class or wrapper class
+public class BinaryNode
+{
+    private int data;// can be replaced by any class or wrapper class
     private BinaryNode left;
     private BinaryNode right;
 
-    public BinaryNode(Object data) {
-        this.data = data;
-        /*
-         * just overstating the obvious -- objects are null by default this.left = null;
-         * this.right = null;
-         */
+    public BinaryNode(int data)
+    {   this.data = data;
+        /* just overstating the obvious -- objects are null by default
+        this.left = null;
+        this.right = null;
+        */
     }
 
-    public Object getData() {
-        return data;
+    public int getData()
+    {   return data;
     }
 
-    public void setData(Object data) {
-        this.data = data;
+    public void setData(int data)
+    {   this.data = data;
     }
 
-    public BinaryNode getLeft() {
-        return left;
+    public BinaryNode getLeft()
+    {   return left;
     }
 
-    public void setLeft(BinaryNode left) {
-        this.left = left;
+    public void setLeft(BinaryNode left)
+    {   this.left = left;
     }
 
-    public BinaryNode getRight() {
-        return right;
+    public BinaryNode getRight()
+    {   return right;
     }
 
-    public void setRight(BinaryNode right) {
-        this.right = right;
+    public void setRight(BinaryNode right)
+    {   this.right = right;
     }
 
     @Override
-    public String toString() {
-        return "BinaryNode.data=" + data;
+    public String toString()
+    {   return String.format("(%d)", data);
     }
 
-    public static void main(String[] args) {
-        Integer i = new Integer(5);
-        BinaryNode n = new BinaryNode(i);
+    /*
+    public static void main(String[] args)
+    {
+        BinaryNode n = new BinaryNode(5);
         System.out.println(n);
     }
+    */
+
 }
