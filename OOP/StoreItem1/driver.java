3dcecaf7d2d1fdb6e4de8a02e83e7e0dd4fc94be
Ron-it <ronitbuti.ofs@gmail.com>
2021-09-22 21:25:13 +0800

    cleanup

--- OOP/StoreItem1/driver.java
+++
@@ -1,8 +0,0 @@
-public class driver {
-
-    public static void main(String[] args) {
-        StoreItem pencil = new StoreItem("pencil", 0.7, "H1", false);
-        StoreItem thermo = new StoreItem("thermometer", 0.7, "H1", false);
-    }
-
-}
